b9fb18f3237d96b7615b27986d007f4fd10b342f
minje <minje1409@gmail.com>
2022-07-19 22:54:31 +0900

    soonsoo

--- day06.js
+++ day06.js
@@ -63,38 +63,64 @@
 //   const printHong = printInfo.bind(hong);
 //   printHong('Kil-dong');
 
-/*
+
 const weeks = ['일', '월', '화', '수', '목', '금', '토'];
-let widx = -1;
-const getNextWeek = () => {
-    widx += 1; // side-effect!
-    if (widx >= weeks.length) widx = 0;
-    return `${weeks[widx]}요일`;
-};
+const getNextWeek = (() => {
+    let widx = -1;
+    return () => {
+        widx += 1; // side-effect!
+        if (widx >= weeks.length) widx = 0;
+        return `${weeks[widx]}요일`;
+    }
+})();
 
 let cnt = 0;
 const intl = setInterval(() => {
     // widx += 2; // side-effect!
     console.log('call', cnt, getNextWeek());
-    if ((cnt += 1) === 8) clearInterval(intl);
-}, 1000);
-*/
-
-const weeks = ['일', '월', '화', '수', '목', '금', '토'];
-let widx = -1; // 클로저를 써서 짜라 / 근데 클로저 안써도 쓸수있다고 하면 해바라~
-const getNextWeek = () => {
-    // widx += 1; // side-effect!
-    let widx_ = 0;
-    return function(n) {
-        widx_ += n;
-        if (widx_ >= weeks.length) widx_ = 0;
-        return `${weeks[widx_]}요일`;
-    }
-};
-
-let cnt = 0;
-const intl = setInterval(() => {
-    widx += 2; // side-effect!
-    console.log('call', cnt, getNextWeek()(cnt));
-    if ((cnt += 1) === 8) clearInterval(intl);
+    if ((cnt += 1) === 20) clearInterval(intl);
 }, 100);
+
+
+// const weeks = ['일', '월', '화', '수', '목', '금', '토'];
+// let widx = -1; 
+// const getNextWeek = () => {
+//     // widx += 1; // side-effect!
+//     return function(n) {
+//         widx += n;
+//         if (widx >= weeks.length) widx = 0;
+//         return `${weeks[widx]}요일`;}
+// };
+
+// let cnt = 0;
+// const intl = setInterval(() => {
+//     widx += 2; // side-effect!
+//     console.log('call', cnt, getNextWeek()(cnt));
+//     if ((cnt += 1) === 8) clearInterval(intl);
+// }, 100);
+
+// const weeks = ['일', '월', '화', '수', '목', '금', '토'];
+// let widx = -1; // 클로저를 써서 짜라, 근데 클로저 안써도 쓸수있다고 하면 해 봐라
+// const getNextWeek = (n) => {
+//     widx += 1; // side-effect!
+//     intl._idleStart = n;
+//     return `${weeks[n]}요일`;
+// };
+
+// let cnt = 0;
+// const intervalTime = 100;
+// const intl = setInterval(() => {
+//     widx += 2; // side-effect!
+//     const num = Math.trunc(intl._idleStart / intervalTime) % weeks.length; // 인터벌 100으로 100일전에 하루 건너 뜀. -> 오류
+
+//     // debugging
+//     if (cnt%weeks.length !== num) {
+//         console.log(cnt, num);
+//         clearInterval(intl);
+//     }
+
+//     console.log('call', cnt, getNextWeek(num), num, intl._idleStart); 
+//     // console.log('call', cnt, num, getNextWeek(num), intl._idleStart); 
+//     if ((cnt += 1) === 10) clearInterval(intl);
+// }, intervalTime);
+
